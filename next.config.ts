f5e974323143696005719527d5e84139f3e822ef
Farras <115802691+miie-fa@users.noreply.github.com>
2024-12-29 13:01:14 +0700

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,12 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  ignoreDuringBuilds: true,
+  /* config options here */
+  eslint: {
+    // Peringatan: Ini memungkinkan build produksi berhasil meskipun
+    // proyek Anda memiliki kesalahan ESLint.
+    ignoreDuringBuilds: true,
+  },
 };
 
 export default nextConfig;
